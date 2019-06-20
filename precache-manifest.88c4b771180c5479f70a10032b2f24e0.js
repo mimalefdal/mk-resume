febaa0ce7ab84dd6c4da0895d1eed1f7b6dc3687
unknown <mimalefdal@yahoo.com>
2019-06-21 02:13:31 +0430

    Updates

---
+++ precache-manifest.88c4b771180c5479f70a10032b2f24e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77ccf2ffa9740a476a2eef3c8e1025b",
+    "url": "/mk-resume/index.html"
+  },
+  {
+    "revision": "39d3a4830d6d278f4395",
+    "url": "/mk-resume/static/css/main.f2ca45b2.chunk.css"
+  },
+  {
+    "revision": "122cb04a33cd222791b9",
+    "url": "/mk-resume/static/js/2.9b082b92.chunk.js"
+  },
+  {
+    "revision": "39d3a4830d6d278f4395",
+    "url": "/mk-resume/static/js/main.0ae4c098.chunk.js"
+  },
+  {
+    "revision": "f00c58f1cc9d6351dc11",
+    "url": "/mk-resume/static/js/runtime~main.1ea4a0d4.js"
+  },
+  {
+    "revision": "d4438dbdbd0000f7f30d9b27a8c93c82",
+    "url": "/mk-resume/static/media/arrow-down.d4438dbd.svg"
+  },
+  {
+    "revision": "5436decf172cab13f81420ba39913ddd",
+    "url": "/mk-resume/static/media/magic-wand.5436decf.svg"
+  }
+]);
